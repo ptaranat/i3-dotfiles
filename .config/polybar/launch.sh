2b072f23b0348bb01ba1e29033b4de9a568d2489
ptaranat <panat.taranat@gmail.com>
2020-02-13 10:48:02 -0500

    Make launch.sh work on multi monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,5 +8,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
 polybar -c ~/.config/polybar/config main &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
+
